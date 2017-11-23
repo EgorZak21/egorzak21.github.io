ac5b3965d3dabb79d50566de2e472c7bedc638cd
EgorZak21 <32587059+EgorZak21@users.noreply.github.com>
2017-11-24 00:55:08 +0300

    project2

--- canvas/js/main.js
+++ canvas/js/main.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
+    $('.info').hide();
+    $(window).resize(function(){
+    if($(document).width()>=991){
+        $('.main-set-tabul').show(500);
+    }
+    });
+    
     $('.tab-rel>div:not(":first-of-type")').hide();
     
     $('.main-set-tabul button').each(function(i){
@@ -12,9 +19,40 @@ $(document).ready(function(){
         
         wrap.find('.main-set-tabul button').removeClass('active');
         $(this).addClass('active');
-        
+        if($(document).width()<991){
+            $('.main-set-tabul').toggle(500);
+        }
         wrap.find('.tab-rel>div').hide();
         wrap.find('.tab-rel>div[data-tab='+dataTab+']').show();
     });
-    
+    $('#graf').on('click',function(){
+        if($(document).width()<991){
+            $('.main-set-tabul').toggle(500);
+        }
+    });
+    $('.none-ul li').on('click',function(){
+        $('.none-ul').hide(500);
+    });
+    $('.ulrel button').on('click',function(){
+        $('.none-ul').toggle(500);
+    });
+    $('#parab').on('click',function(){
+        $('.info').hide();
+        $('#parabTab').show();
+    });
+    $('#ellips').on('click',function(){
+        $('.info').hide();
+        $('#ellipsTab').show();
+    });
+    $('#circl').on('click',function(){
+        $('.info').hide();
+        $('#circlTab').show();
+    });
+    $('#giper').on('click',function(){
+        $('.info').hide();
+        $('#giperTab').show();
+    });
+    $('form').submit(function (e) {
+    e.preventDefault();
+});
 }); 
