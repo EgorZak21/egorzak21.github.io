7e318f3356b3c281fda5c8a6d35c03407607d5e8
EgorZak21 <32587059+EgorZak21@users.noreply.github.com>
2019-05-31 04:07:52 +0300

    Added VkTest

---
+++ vktest/precache-manifest.52b14480bcf08796f3e95df27d9e01fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac8b64ba80d4a9bb8e2a4d4100ac1dea",
+    "url": "/vktest/index.html"
+  },
+  {
+    "revision": "6406863b166bcea55d09",
+    "url": "/vktest/static/css/2.fd05c352.chunk.css"
+  },
+  {
+    "revision": "70ac0fda2b623509a3c3",
+    "url": "/vktest/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "6406863b166bcea55d09",
+    "url": "/vktest/static/js/2.e377b206.chunk.js"
+  },
+  {
+    "revision": "70ac0fda2b623509a3c3",
+    "url": "/vktest/static/js/main.e48896dc.chunk.js"
+  },
+  {
+    "revision": "a43572ad29ab9921d6e0",
+    "url": "/vktest/static/js/runtime~main.6ae16690.js"
+  }
+]);
